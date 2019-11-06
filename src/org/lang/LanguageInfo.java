13a9e63103293ee79bcb65745e3161e4e660f32d
ibrahimaA1 <mohammedibrahimji@gmail.com>
2019-11-06 11:07:03 +0530

    has done my user stories

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,17 @@
+package org.lang;
+
+public class LanguageInfo {
+public void tmillanguage() {
+	System.out.println("tamilnadu");
+}
+public void englislanguage() {
+	System.out.println("cmmon");
+}
+public void hindilanguage() {
+	System.out.println("maharastra");
+}
+public static void main(String[] args) {
+	StateDetails l = new StateDetails();
+	l.englislanguage();l.hindilanguage();l.southindia();l.nothindia();l.tmillanguage();
+}
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,11 @@
+package org.lang;
+
+public class StateDetails extends LanguageInfo {
+	public void southindia() {
+		System.out.println("tamilnadu,kerala,karnataka,andhra");
+	}
+	public void nothindia() {
+		System.out.println("newdelhi,mumbai.pune,mp,up");
+	}
+}
+
